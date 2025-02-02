e2b9241f94c9f2fa82eb18c88843d37148895fe5
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-02-02 17:33:07 +0000

    fix build

--- P01_BASIC_PROJECT/app/controllers/HomeController.scala
+++ P01_BASIC_PROJECT/app/controllers/HomeController.scala
@@ -4,11 +4,76 @@ import play.api.*
 import play.api.mvc.*
 
 import javax.inject.*
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Random
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+  val controllerComponents: ControllerComponents,
+  implicit val ect: ExecutionContext
+) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
+
+
+    val rnd = new Random().nextInt(100)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-1----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+
+
+    Future.successful{
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+      println("--" + Thread.currentThread().getName + " @" + rnd)
+      Thread.sleep(1000)
+    }
+
+
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+    println("-2----------------------"+Thread.currentThread().getName + " @"+rnd)
+    Thread.sleep(1000)
+
     Ok(views.html.index())
   }
 }
